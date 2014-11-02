1b785b7bf0836b2d2e3e8f09f33e2879db04d84d
ChenYuHsin <s63232@hotmail.com>
2014-11-03 01:45:55 +0800

    Add ConfirmCircle

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() { 
 
 	// ===========================tabs交換效果
@@ -6,16 +7,12 @@ $(document).ready(function() {
 		//預設幫上面tabs選項第一個加上current這個class
 
 		
-		$('.tabs div a').click(function (g) { 
+		$('.tabs div a').click(function () { 
 		 	var navbar = $(this).closest('.navbar'),            //就navbar 
 		 		index = $(this).closest('div').index();			//點中的那個tab編號(0 or 1)
 		 		         
-
-
 		 	navbar.find('.tabs > div').removeClass('current');  //移除current
 		 	$(this).closest('div').addClass('current');			//幫點中那一個tab加current
-
-
 
 		 	if (index==1) {
 		 		$(".tab0").slideUp(); 
@@ -25,45 +22,78 @@ $(document).ready(function() {
 		 		$(".tab0").slideDown();
 		 	};
 			
-		 	g.preventDefault();                                 //不確定幹嘛...
-		});
-
-		$('.add').click(function() {
-	    	$('.modal-wrapper').toggleClass('open');
-	    	$('.tab0').toggleClass('blur');
-	     	return false;
-	    });
-
-
-     	$('.btn-close').click(function(){
-     		$(".modal-wrapper").removeClass("open");
-     		$(".tab0").removeClass("blur");
-
-		});
-     
-
-     	$('.btn_nextModal').click(function(){
-     		var state="state2";
-     		$(".form_step1").toggleClass(state);
-     		$(".form_step2").toggleClass(state);
-     		$(".btn_submitModal").toggleClass(state);
-     		$(".btn_nextModal").toggleClass(state);
-		});
-
-
-		$(".btn_submitModal").click(function(){
-			var newExamBlock=$('<div class =\"blocks\"></div>');
-			$(".tab0").prepend(newExamBlock);
-			
-     		$(".modal-wrapper").removeClass("open");
-     		$(".tab0").removeClass("blur");
+		 	// g.preventDefault();                                 //不確定幹嘛...
 		});
 });
 
+//================================================================================刪除考試
+$(document).on("click",".trash_can",Show_Confirm);
 
-// 新增==============
+function Show_Confirm(e){
+	Show_ConfirmWrapper();
+	// ===================刪除考試功能
+	var div_clicked=$(e.target).parent().parent('div');
+	$(".inner-square.left").click(function(){
+		div_clicked.remove();
+		Close_ConfirmWrapper();
+	});
+}
 
-// function AddExam(){
-// 	var newExamBlock=$('<div class =\"blocks\"></div>');
-// 	$(".tab0").prepend(newExamBlock);
+$(document).on("click",".inner-square.right",function(){
+	Close_ConfirmWrapper();
+});
+
+function Show_ConfirmWrapper(){
+	$('.tab0').toggleClass('blur');	
+	$(".confirmCircle_wrapper").toggleClass("open");	
+}
+
+function Close_ConfirmWrapper(){
+	$('.tab0').removeClass('blur');	
+	$(".confirmCircle_wrapper").removeClass("open");
+}
+
+// function Delete_Exam(e){           //用e指定滑鼠點擊目標
+// 	$(e.target).parent().parent('div').remove();	
 // }
+
+
+//=================================================================================新增考試
+$(document).on("click",".add",function(){
+	Open_ModalWrapper();
+});
+$(document).on("click",".btn_nextModal",function(){
+	var nowState="state2";
+	$(".form_step1").toggleClass(nowState);
+	$(".form_step2").toggleClass(nowState);
+	$(".btn_submitModal").toggleClass(nowState);
+	$(".btn_nextModal").toggleClass(nowState);
+});
+$(document).on("click",".btn_submitModal",function(){
+	AddExam();
+	Close_ModalWrapper();
+});
+$(document).on("click",".btn_close",function(){
+	Close_ModalWrapper();
+});
+
+function Open_ModalWrapper(){
+	$('.modal-wrapper').toggleClass('open');
+	$('.tab0').toggleClass('blur');	
+}
+function Close_ModalWrapper(){
+	$(".modal-wrapper").removeClass("open");
+	$(".tab0").removeClass("blur");
+	Back_To_State1();
+}
+function Back_To_State1(){
+	$(".form_step1").removeClass("state2");
+	$(".btn_nextModal").removeClass("state2");
+	$(".form_step2").removeClass("state2");
+	$(".btn_submitModal").removeClass("state2");
+}
+function AddExam(){
+ 	var newExamBlock=$('<div class ="blocks"><div class="icon-button three_points"><core-icon icon="more-vert"></core-icon><paper-ripple class="circle recenteringTouch" fit></paper-ripple></div><div class="icon-button trash_can"><core-icon icon="delete"></core-icon><paper-ripple class="circle recenteringTouch" fit></paper-ripple></div> <div class="fab green"><core-icon icon="create"></core-icon><paper-ripple class="circle recenteringTouch" fit></paper-ripple></div></div>');
+	$(".tab0").prepend(newExamBlock);
+ }
+
