e4f004f208264ddad009686596d1ed8fda8f4de5
ChenYuHsin <s63232@hotmail.com>
2014-11-04 22:09:23 +0800

    backup

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 
 $(document).ready(function() { 
 
-	// ===========================tabs交換效果
+	    // ================================================================tabs交換效果
 		var tab_now;
 		$('.navbar').addClass('active').find('> .tabs div:eq(0)').addClass('current');
 		//預設幫上面tabs選項第一個加上current這個class
-
 		
 		$('.tabs div a').click(function () { 
 		 	var navbar = $(this).closest('.navbar'),            //就navbar 
@@ -15,41 +14,41 @@ $(document).ready(function() {
 		 	$(this).closest('div').addClass('current');			//幫點中那一個tab加current
 
 		 	if (index==1) {
-		 		$(".tab0").slideUp(); 
-		 		$(".tab1").slideDown();
+		 		$(".tab_exams").slideUp(); 
+		 		$(".tab_questions").slideDown();
 		 	} else{
-		 		$(".tab1").slideUp(); 
-		 		$(".tab0").slideDown();
+		 		$(".tab_questions").slideUp(); 
+		 		$(".tab_exams").slideDown();
 		 	};
 			
 		 	// g.preventDefault();                                 //不確定幹嘛...
 		});
 });
 
-//================================================================================刪除考試
+//=========================================================================刪除考試，按下垃圾桶
 $(document).on("click",".trash_can",Show_Confirm);
 
 function Show_Confirm(e){
 	Show_ConfirmWrapper();
-	// ===================刪除考試功能
+	// ------------------------------------------------------------刪除block
 	var div_clicked=$(e.target).parent().parent('div');
-	$(".inner-square.left").click(function(){
+	$(".inner-square.left").click(function(){   //點左邊，確認刪除
 		div_clicked.remove();
 		Close_ConfirmWrapper();
 	});
 }
-
+//----------------------------------------------------------------點右邊沒事
 $(document).on("click",".inner-square.right",function(){
 	Close_ConfirmWrapper();
 });
 
 function Show_ConfirmWrapper(){
-	$('.tab0').toggleClass('blur');	
+	$('.tab_exams').toggleClass('blur');	
 	$(".confirmCircle_wrapper").toggleClass("open");	
 }
 
 function Close_ConfirmWrapper(){
-	$('.tab0').removeClass('blur');	
+	$('.tab_exams').removeClass('blur');	
 	$(".confirmCircle_wrapper").removeClass("open");
 }
 
@@ -79,11 +78,11 @@ $(document).on("click",".btn_close",function(){
 
 function Open_ModalWrapper(){
 	$('.modal-wrapper').toggleClass('open');
-	$('.tab0').toggleClass('blur');	
+	$('.tab_exams').toggleClass('blur');	
 }
 function Close_ModalWrapper(){
 	$(".modal-wrapper").removeClass("open");
-	$(".tab0").removeClass("blur");
+	$(".tab_exams").removeClass("blur");
 	Back_To_State1();
 }
 function Back_To_State1(){
@@ -94,6 +93,6 @@ function Back_To_State1(){
 }
 function AddExam(){
  	var newExamBlock=$('<div class ="blocks"><div class="icon-button three_points"><core-icon icon="more-vert"></core-icon><paper-ripple class="circle recenteringTouch" fit></paper-ripple></div><div class="icon-button trash_can"><core-icon icon="delete"></core-icon><paper-ripple class="circle recenteringTouch" fit></paper-ripple></div> <div class="fab green"><core-icon icon="create"></core-icon><paper-ripple class="circle recenteringTouch" fit></paper-ripple></div></div>');
-	$(".tab0").prepend(newExamBlock);
+	$(".tab_exams").prepend(newExamBlock);
  }
 
