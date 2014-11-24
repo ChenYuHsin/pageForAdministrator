6eaf7c2c1e1be77daa2efbd8c36760258ccb674f
ChenYuHsin <s63232@hotmail.com>
2014-11-24 10:23:16 +0800

    update

--- js/index.js
+++ js/index.js
@@ -229,9 +229,9 @@ function AddExam(){
  }
 
 function onlyNum(){  //限制input輸入數字
-	if(!(event.keyCode==46)&&!(event.keyCode==8)&&!(event.keyCode==37)&&!(event.keyCode==39))
-	if(!((event.keyCode>=48&&event.keyCode<=57)||(event.keyCode>=96&&event.keyCode<=105)))
-	event.returnValue=false;
+    if(!(event.keyCode==46)&&!(event.keyCode==8)&&!(event.keyCode==37)&&!(event.keyCode==39))
+    if(!((event.keyCode>=48&&event.keyCode<=57)||(event.keyCode>=96&&event.keyCode<=105)))
+    event.returnValue=false;
 }
 
 
